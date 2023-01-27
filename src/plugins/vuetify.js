187144f74b2f98f51081204e63b415bab8ed57f5
arthurstofeles <arthur.stofeles.web@gmail.com>
2023-01-26 21:58:39 -0300

    configurando o theme color do Vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,18 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+          light: {
+            yt_red: "#FF0000",
+            yt_ligth_blue: "#def1ff",
+            yt_blue: "#065fd4",
+            yt_black: "#0f0f0f",
+            yt_gray: "#cccccc",
+            yt_ligth_gray: "rgba(0,0,0,0.5)",
+            yt_white: "#ffffff"
+          },
+        },
+      },
+    
 });
