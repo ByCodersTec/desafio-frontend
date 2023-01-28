2f2c5475250d737055f0a5f129ac46859ee82bf9
arthurstofeles <arthur.stofeles.web@gmail.com>
2023-01-28 15:24:24 -0300

    criando a componente de navegacao mobile

---
+++ src/layouts/BottomBarNavegation.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="pa-1 yt_white bottom-bar">
+    <div
+      v-for="item in items"
+      :key="item.title"
+      link
+      @click="goTo(item.route)"
+      class="d-flex flex-column align-center"
+    >
+    <v-icon color="yt_black">{{ item.icon }}</v-icon>
+    <span class="caption">{{ item.title }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BottomBarNavegation",
+  data() {
+    return {
+      items: [{ title: "Home", icon: "mdi-home", route: "Home" }],
+    };
+  },
+  methods: {
+    goTo(route) {
+      this.$router.push({ name: route });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.bottom-bar {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 9;
+    box-shadow: 0px -5px 10px 2px rgba(0,0,0,0.1);
+}
+</style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,19 +3,21 @@
     <Header @openSidebar="sidebar = !sidebar" :isMobile="isMobile" />
     <div class="layout">
       <SideBarNavegation v-if="!isMobile" :sidebar="sidebar" />
-      <v-container fluid class="pa-4">
+      <v-container fluid class="pa-4 mb-12">
         <router-view />
       </v-container>
     </div>
+    <BottomBarNavegation v-if="isMobile" />
   </div>
 </template>
 
 <script>
 import Header from "../layouts/Header.vue";
 import SideBarNavegation from "../layouts/SideBarNavegation.vue";
+import BottomBarNavegation from "../layouts/BottomBarNavegation.vue";
 
 export default {
-  components: { Header, SideBarNavegation },
+  components: { Header, SideBarNavegation, BottomBarNavegation },
   name: "Default",
   data() {
     return {
